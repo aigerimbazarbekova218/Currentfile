0d43deda9ecd4bcbe2c757a550a12058973bc0dd
aiger <aigerim.bazarbekova218@gmail.com>
2023-12-06 21:14:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,65 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        byte a=87;
+        byte b=43;
+        int c=56;
+        int d=44;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println("Бул сандардын суммасы:"+(a+b));
+        System.out.println("Бул сандардын кобойтундусу:"+(a*b));
+        System.out.println("Бул сандардын айырмасы:"+(a-b));
+        System.out.println("Бул сандардын болуусу:"+(a/b));
+
+        System.out.println("Бул сандардын суммасы:"+(c+d));
+        System.out.println("Бул сандардын кобойтундусу:"+(c*d));
+        System.out.println("Бул сандардын айырмасы:"+(c-d));
+        System.out.println("Бул сандардын болуусу:"+(c/d));
+
+
+        byte q=100;
+        byte w=89;
+        byte e=44;
+        byte r=67;
+        byte t=34;
+        short y=10768;
+        short u=12456;
+        short i=23987;
+        short o=31445;
+        short l=4322;
+        int s=34445566;
+        int f=23245;
+        int g=34356;
+        int h=45366;
+        int k=45567;
+        long x=334555665;
+        long v=345615555;
+        long n=553227788;
+        long m=576458653;
+        long z=654635654;
+        System.out.println(q);
+        System.out.println(w);
+        System.out.println(e);
+        System.out.println(r);
+        System.out.println(t);
+        System.out.println(y);
+        System.out.println(u);
+        System.out.println(i);
+        System.out.println(o);
+        System.out.println(l);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(k);
+        System.out.println(x);
+        System.out.println(v);
+        System.out.println(n);
+        System.out.println(m);
+        System.out.println(z);
+    }
+    
+}
